599c6b44bd2171bb5b7fe2d18e71e9346ea88c20
sammypijamy <samaragransky@outlook.com>
2022-08-30 18:43:40 -0700

    commit tree 3c4b7e002cbe2cd1961e6702251e506a6c3a0a70

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+    window.customerName = 'BOB'
+}
+
+var bestCustomer 
+function setBestCustomer() {
+  return  window.bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer() {
+    return window.bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'jr'
+function changeLeastFavoriteCustomer() {
+    return leastFavoriteCustomer = 'sam'
+}
